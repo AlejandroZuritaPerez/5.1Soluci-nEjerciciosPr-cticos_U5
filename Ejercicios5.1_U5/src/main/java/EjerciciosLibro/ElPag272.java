8152544bc125719308ddad47dec24b4f259733ba
AlejandroZuritaPerez <119274449+AlejandroZuritaPerez@users.noreply.github.com>
2023-05-14 18:45:34 -0600

    Add files via upload

---
+++ Ejercicios5.1_U5/src/main/java/EjerciciosLibro/ElPag272.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjerciciosLibro;
+
+/**
+ *
+ * @author ALEJANDRO ZURITA PÉREZ
+ */
+public class ElPag272 {
+    
+    private String cara;
+    private String palo;
+
+    public ElPag272(String cara, String palo) {
+        this.cara = cara;
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        return cara + " de " + palo;
+    }
+    
+    
+}
